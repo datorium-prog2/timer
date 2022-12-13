097b3e48dbcd2b08588d30f2bb239e8cf9d5ca86
Serzsh <serzants.janis@gmail.com>
2022-12-13 15:08:26 +0200

    Added comments

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 class Timer {
   constructor(containerSelector) {
+    // paņemsim wrapper elementu no dokumenta objekta
     this.timerWrapper = document.querySelector(containerSelector)
+    // paņemsim pārējos elementus no wrapper
     this.startButton = this.timerWrapper.querySelector('.js-start')
     this.minutesLeft = this.timerWrapper.querySelector('.js-minutes')
     this.secondsLeft = this.timerWrapper.querySelector('.js-seconds')
 
+    // sākumā uz lapas ielādi ir 10min, jeb 600s
     this.cuurentTimeInSeconds = 600
     
-
+    // pasakam uzreiz, kad nospiežam pogu tad kaut ko darām
     this.startButton.addEventListener('click', () => {
       console.log(`clicked and current time is ${this.cuurentTimeInSeconds}`)
     })
@@ -17,5 +20,5 @@ class Timer {
 
 }
 
-
+// Enkapsukēsim visu timeri zem viena wrappera
 const timer = new Timer('.js-timer')
