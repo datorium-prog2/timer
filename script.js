4aa3cd7c7e7cb705b5e5a24bce1ef2670ec5ed0c
Serzsh <serzants.janis@gmail.com>
2022-12-19 14:53:10 +0200

    Added abstractions

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ class Timer {
     this.minutesLeftElement = this.timerWrapper.querySelector('.js-minutes')
     this.secondsLeftElement = this.timerWrapper.querySelector('.js-seconds')
 
+    // intervāla identifikators, lai varētu viņu vēlāk apturēt
     this.intervalId = null
 
     // sākumā uz lapas ielādi ir 10min, jeb 600s
@@ -19,21 +20,53 @@ class Timer {
     // aizpildam HTML ar MM un SS vērtībām
     this.setMinutesAndSeconds()
 
+    // izsaucam metodi, kura pieliek pogām click eventu
+    this.addStartEvent()
+  }
+
+  addStartEvent() {
     // pasakam uzreiz, kad nospiežam pogu tad kaut ko darām
     this.startButton.addEventListener('click', () => {
-      this.intervalId = setInterval(() => {
-        this.cuurentTimeInSeconds = this.cuurentTimeInSeconds - 1
-        this.setMinutesAndSeconds()
-      }, 1000)   
+      // ja intervāls jau ir aktīvs tad apturam, ja nē tad sākam
+      if(this.intervalId) {
+        // iztīram esošo intervālu
+        this.clearCurrentInterval()
+      } else {
+        // izveidojam jaunu intervālu
+        this.setCurrentInterval()
+      }
     })
+  }
 
-    this.stopButton.addEventListener('click', () => {
-      clearInterval(this.intervalId)
-      this.intervalId = null
-    })
+  setCurrentInterval() {
+    // izveidojam intervālu, kurš skaita katras 1000ms jeb sekundi
+    this.intervalId = setInterval(() => {
+      // samazinam laiku par vienu sekundi
+      this.cuurentTimeInSeconds = this.cuurentTimeInSeconds - 1
+      // liekam klasei atkal iesetot aktuālās sekundes
+      this.setMinutesAndSeconds()
+    }, 1000)   
+
+    // nomainam pogas tekstu
+    this.addtextToButton('Stop Timer')
+
+  }
+
+  clearCurrentInterval() {
+    // apturam intervālu pēc identifikatora, kuru iesetojam tad kad izveidojam intervalu
+    clearInterval(this.intervalId)
+    // ja mēs iztīram intervālu tad atgriežam intervalId uz null
+    this.intervalId = null
+
+    this.addtextToButton('Start Timer')
+  }
+
+  addtextToButton(text) {
+    this.startButton.innerHTML = text
   }
   
   setMinutesAndSeconds() {
+    // iesetojam vērtības mūsu HTMLā
     this.minutesLeftElement.innerHTML = this.wholeMinutes()
     this.secondsLeftElement.innerHTML = this.wholeSeconds()
   }
@@ -53,3 +86,11 @@ class Timer {
 
 // Enkapsukēsim visu timeri zem viena wrappera
 const timer = new Timer('.js-timer')
+
+
+// Kas notiek, kad timeris sasniedz 0 sekundes
+// Var atkārtoti spaidīt start pogu
+// Reset opciju ieviest
+// ievadīt savu laiku
+// kaut kāds paziņojums, tad kad laiks ir beidzies
+//
