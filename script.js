f7e652887f2be5dfd7f52ab5a0879fff0e053c73
Serzsh <serzants.janis@gmail.com>
2022-12-19 13:50:53 +0200

    updated code

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 class Timer {
+  secondsInOneMinute = 60
+
   constructor(containerSelector) {
     // paņemsim wrapper elementu no dokumenta objekta
     this.timerWrapper = document.querySelector(containerSelector)
+
     // paņemsim pārējos elementus no wrapper
     this.startButton = this.timerWrapper.querySelector('.js-start')
-    this.minutesLeft = this.timerWrapper.querySelector('.js-minutes')
-    this.secondsLeft = this.timerWrapper.querySelector('.js-seconds')
+    this.minutesLeftElement = this.timerWrapper.querySelector('.js-minutes')
+    this.secondsLeftElement = this.timerWrapper.querySelector('.js-seconds')
 
     // sākumā uz lapas ielādi ir 10min, jeb 600s
-    this.cuurentTimeInSeconds = 600
+    this.cuurentTimeInSeconds = 599
     
     // pasakam uzreiz, kad nospiežam pogu tad kaut ko darām
     this.startButton.addEventListener('click', () => {
       console.log(`clicked and current time is ${this.cuurentTimeInSeconds}`)
     })
 
-    console.log(this);
+    this.minutesLeftElement.innerHTML = this.wholeMinutes()
+    this.secondsLeftElement.innerHTML = this.wholeSeconds()
   }
 
+  timeInSecondsDivideByOneMin() {
+    return (this.cuurentTimeInSeconds / this.secondsInOneMinute) 
+  }
+
+  wholeMinutes() {
+    return Math.floor(this.timeInSecondsDivideByOneMin());
+  }
+
+  wholeSeconds() {
+    return this.cuurentTimeInSeconds % this.secondsInOneMinute
+  }
 }
 
 // Enkapsukēsim visu timeri zem viena wrappera
