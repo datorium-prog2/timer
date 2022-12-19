70cc010caa898efb55515f1ea591fa88eb5fb640
Serzsh <serzants.janis@gmail.com>
2022-12-19 14:27:51 +0200

    Made timer to work

--- script.js
+++ script.js
@@ -7,17 +7,33 @@ class Timer {
 
     // paņemsim pārējos elementus no wrapper
     this.startButton = this.timerWrapper.querySelector('.js-start')
+    this.stopButton = this.timerWrapper.querySelector('.js-stop')
     this.minutesLeftElement = this.timerWrapper.querySelector('.js-minutes')
     this.secondsLeftElement = this.timerWrapper.querySelector('.js-seconds')
 
+    this.intervalId = null
+
     // sākumā uz lapas ielādi ir 10min, jeb 600s
-    this.cuurentTimeInSeconds = 599
-    
+    this.cuurentTimeInSeconds = 600
+
+    // aizpildam HTML ar MM un SS vērtībām
+    this.setMinutesAndSeconds()
+
     // pasakam uzreiz, kad nospiežam pogu tad kaut ko darām
     this.startButton.addEventListener('click', () => {
-      console.log(`clicked and current time is ${this.cuurentTimeInSeconds}`)
+      this.intervalId = setInterval(() => {
+        this.cuurentTimeInSeconds = this.cuurentTimeInSeconds - 1
+        this.setMinutesAndSeconds()
+      }, 1000)   
     })
 
+    this.stopButton.addEventListener('click', () => {
+      clearInterval(this.intervalId)
+      this.intervalId = null
+    })
+  }
+  
+  setMinutesAndSeconds() {
     this.minutesLeftElement.innerHTML = this.wholeMinutes()
     this.secondsLeftElement.innerHTML = this.wholeSeconds()
   }
